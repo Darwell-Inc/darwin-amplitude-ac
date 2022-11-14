feb350b67c55b7329980eb0687d9bef4e546714a
Ivan O <ioparin@pm.me>
2022-11-14 17:10:57 +0400

    disabling broken swift package test target

--- Package.swift
+++ Package.swift
@@ -1,5 +1,4 @@
 // swift-tools-version:5.3
-// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
@@ -12,25 +11,22 @@ let package = Package(
         .watchOS(.v3)
     ],
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "AnalyticsConnector",
             targets: ["AnalyticsConnector"]
         ),
     ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-    ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         .target(
             name: "AnalyticsConnector",
             path: "Sources/AnalyticsConnector",
-            exclude: ["Info.plist"]),
-        .testTarget(
-            name: "AnalyticsConnectorTests",
-            dependencies: ["AnalyticsConnector"],
-            path: "Tests/AnalyticsConnectorTests",
-            exclude: ["Info.plist"]),
+            exclude: ["Info.plist"]
+		),
+//        .testTarget(
+//            name: "AnalyticsConnectorTests",
+//            dependencies: ["AnalyticsConnector"],
+//            path: "Tests/AnalyticsConnectorTests",
+//            exclude: ["Info.plist"]
+//		),
     ]
 )
